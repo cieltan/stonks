228d4345fde3584cddc678c62d79e6ce6d2803af
btan <btan7645@gmail.com>
2020-02-11 17:09:46 -0500

    feat: Configure babel/webpack for front-end bundling/transpiling

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./client/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: [
+          {
+            loader: "babel-loader",
+            query: {
+              presets: ["@babel/preset-react"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
