c96c4d8768533607df7d49b48134a1c5dadfaa0d
btan <btan7645@gmail.com>
2020-02-10 23:45:11 -0500

    feat: Implement basic express server and eslint/prettier configs

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+const createApp = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(morgan("dev"));
+
+  app.use(express.static(path.join(__dirname, "..", "public")));
+
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  });
+
+  app.use((err, req, res) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
+
+const startListening = () => {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+};
+
+async function startApp() {
+  await createApp();
+  await startListening();
+}
+
+startApp();
+
+module.exports = app;
