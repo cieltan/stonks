52a64a220e883ca26732500b3f999ae13d88d3f9
btan <btan7645@gmail.com>
2020-02-13 11:52:15 -0500

    fix(api): Fix import error from removing alpha

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const portfolio = require("./portfolio");
 const transaction = require("./transaction");
-const alpha = require("./alpha");
 
 router.use("/portfolio", portfolio);
 router.use("/transaction", transaction);
-router.use("/alpha", alpha);
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
